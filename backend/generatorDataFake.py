a2b65c0025c116897c10a59e8adedbf7e1648116
Lucas Martins <lucasmartins1709@hotmail.com>
2024-06-04 23:10:48 -0300

    Apenas arquivos de geração de dados fake

---
+++ backend/generatorDataFake.py
@@ -0,0 +1,25 @@
+import json
+from faker import Faker
+from datetime import datetime
+
+# Inicializando o Faker
+fake = Faker()
+
+# Quantidade de registros a serem gerados
+num_records = 5
+
+# Gerando os dados fictícios
+data = []
+for _ in range(num_records):
+    record = {
+        "username": fake.name(),
+        "email": fake.email(),
+        "password": fake.password()
+    }
+    data.append(record)
+
+# Salvando os dados em um arquivo JSON
+with open('fake_data.json', 'w') as json_file:
+    json.dump(data, json_file, indent=4)
+
+print(f'{num_records} registros foram salvos no arquivo fake_data.json')
